f34265184383563f3121925ed157a49a1ce49ab4
Omeraydognn <fastmers44@gmail.com>
2026-08-14 18:46:11 +0300

    Update navigation bar design

--- packages/web/src/components/Nav.tsx
+++ packages/web/src/components/Nav.tsx
@@ -1,48 +1,23 @@
-import { shortAddress } from "../lib/wallet";
-
-interface NavProps {
-  address: string | null;
-  onConnect: () => void;
-}
-
-export function Nav({ address, onConnect }: NavProps) {
+export function Nav() {
   return (
     <nav className="nav section">
       <div className="nav__brand">
-        <div className="nav__dots" aria-hidden>
-          <span />
-          <span className="off" />
-          <span />
-          <span className="off" />
-          <span />
-          <span className="off" />
-          <span />
-          <span className="off" />
-          <span />
+        <div className="nav__dots">
+          <div className="nav__dot"></div>
+          <div className="nav__dot"></div>
+          <div className="nav__dot"></div>
+          <div className="nav__dot"></div>
+          <div className="nav__dot"></div>
+          <div className="nav__dot"></div>
+          <div className="nav__dot"></div>
+          <div className="nav__dot"></div>
+          <div className="nav__dot"></div>
         </div>
         <span className="nav__word">veriarfy</span>
       </div>
-
       <div className="nav__links">
-        <a className="nav__link" href="#katil">
-          Katil
-        </a>
-        <a className="nav__link" href="#sonuclar">
-          Sonuclar
-        </a>
-      </div>
-
-      <div className="nav__actions">
-        {address ? (
-          <span className="tag">{shortAddress(address)}</span>
-        ) : (
-          <button className="pill pill--ghost" onClick={onConnect}>
-            Cuzdan
-          </button>
-        )}
-        <a className="pill pill--primary" href="#katil">
-          Katil
-        </a>
+        <a href="#anket" className="nav__link">Ankete Katıl</a>
+        <a href="#sonuclar" className="nav__link">Sonuçlar</a>
       </div>
     </nav>
   );
