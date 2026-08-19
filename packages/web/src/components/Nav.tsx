ccec02780954d427c71bd5b0adb1214a61047a86
Omeraydognn <fastmers44@gmail.com>
2026-08-20 00:29:48 +0300

    Update UI component: Nav.tsx

--- packages/web/src/components/Nav.tsx
+++ packages/web/src/components/Nav.tsx
@@ -16,7 +16,8 @@ export function Nav() {
         <span className="nav__word">veriarfy</span>
       </div>
       <div className="nav__links">
-        <a href="#anket" className="nav__link">Ankete Katıl</a>
+        <a href="#durum" className="nav__link">Sistem Durumu</a>
+        <a href="#katil" className="nav__link">Katıl</a>
         <a href="#sonuclar" className="nav__link">Sonuçlar</a>
       </div>
     </nav>
